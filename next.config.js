078627193e0b69e7045ce121fa502cd3acf68aab
[Bruno] <[bruno.micalli@uol.com.br]>
2021-12-21 23:45:15 -0300

    Teste husky

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+const withSass = require('@zeit/next-sass')
 module.exports = {
   reactStrictMode: true,
 }
+
+module.exports = withSass({
+  cssModules: true
+})
